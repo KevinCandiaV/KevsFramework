374ccdba9647cfdb84e7b6e2528ffe6e0051bc3f
Kevin Candia Villagomez <kacandia@vectoritcgroup.com>
2021-09-22 01:19:37 -0500

    first commit

---
+++ KevsFramework/KevsFramework/KevsFramework.h
@@ -0,0 +1,18 @@
+//
+//  KevsFramework.h
+//  KevsFramework
+//
+//  Created by vector on 22/09/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KevsFramework.
+FOUNDATION_EXPORT double KevsFrameworkVersionNumber;
+
+//! Project version string for KevsFramework.
+FOUNDATION_EXPORT const unsigned char KevsFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KevsFramework/PublicHeader.h>
+
+
---
+++ KevsFramework/KevsFramework/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  KevsFramework
+//
+//  Created by vector on 22/09/21.
+//
+
+import Foundation
+
+public class Droppable {
+    
+    private init() {}
+    
+    public static func doSomething() -> String {
+        return "Boi!"
+    }
+    
+}
